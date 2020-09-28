ec97907243af7d2d3b62a02de5589bf80d4ec770
Supraja <suprajasridhara@gmail.com>
2020-09-28 16:52:55 +0000

    run pln propogator on ticker

--- go/pln/plncomm/plnhandlers.go
+++ go/pln/plncomm/plnhandlers.go
@@ -10,5 +10,9 @@ type PLNListHandler struct {
 
 func (p PLNListHandler) Handle(r *infra.Request) *infra.HandlerResult {
 	log.Info("Entering: PLNListHandler.Handle")
+	//verify signature on the message
+
+	//verify signatures for every plnListEntry before inserting into db
+
 	return nil
 }
