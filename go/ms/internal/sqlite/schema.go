2627ce097829e3a8ce3812a210fadd9351e75a5f
suprajasridhara <suprajasridhara@gmail.com>
2020-12-01 12:02:20 +0100

    Update go/ms/internal/sqlite/schema.go
    Co-authored-by: FR4NK-W <wirzf@inf.ethz.ch>

--- go/ms/internal/sqlite/schema.go
+++ go/ms/internal/sqlite/schema.go
@@ -17,7 +17,7 @@ const (
 	/*Schema for MS:
 	- full_map - stores the processed mapping entries that the MS
 		pulls from the Publishing Infrastructure
-	- new_entries - stores new mappings that MS recives from SIGs
+	- new_entries - stores new mappings that MS receives from SIGs
 		to be pushed to the Publishing Infrastructure
 	- pcn_reps - stores response tokens from the publishing
 		infrastructure.
