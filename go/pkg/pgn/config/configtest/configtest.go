31c36e06d3a0b44ae6340c9b70549c5d30572277
Supraja <suprajasridhara@gmail.com>
2021-01-11 10:26:43 +0000

    PR comment resolution: adding copyright and fixing linter reported errors

--- go/pkg/pgn/config/configtest/configtest.go
+++ go/pkg/pgn/config/configtest/configtest.go
@@ -1,12 +1,27 @@
+// Copyright 2021 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package configtest
 
 import (
 	"net"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/scionproto/scion/go/lib/xtest"
 	"github.com/scionproto/scion/go/pkg/ms/config"
-	"github.com/stretchr/testify/assert"
 )
 
 func CheckTestPGN(t *testing.T, cfg *config.PgnConf, id string) {
